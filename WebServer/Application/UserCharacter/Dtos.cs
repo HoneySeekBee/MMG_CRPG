4dfdfa8d45dc7ace7deaeae0febfad5d6614ebb8
HoneySeekBee <dosik112290@gmail.com>
2025-10-22 17:10:57 +0900

    [MMG_CRPG] [Client + WebAPI] 유저 보유 캐릭터 장비 #1 테이블 설계 및 엔티티로 받아오기

--- WebServer/Application/UserCharacter/Dtos.cs
+++ WebServer/Application/UserCharacter/Dtos.cs
@@ -9,15 +9,20 @@ namespace Application.UserCharacter
 {
     public sealed record UserCharacterDto(
     int UserId, int CharacterId, int Level, int Exp, int BreakThrough, DateTimeOffset UpdatedAt,
-    IReadOnlyList<UserCharacterSkillDto> Skills);
+    IReadOnlyList<UserCharacterSkillDto> Skills,
+    IReadOnlyList<UserCharacterEquipDto> equips);
 
     public sealed record UserCharacterSkillDto(int SkillId, int Level, DateTimeOffset UpdatedAt);
+
+    public sealed record UserCharacterEquipDto(int UserId, int Characterid, short slotId, int? ItemId);
+
     public static class UserCharacterMappings
     {
         public static UserCharacterDto ToDto(this UserCha e) =>
             new(
                 e.UserId, e.CharacterId, e.Level, e.Exp, e.BreakThrough, e.UpdatedAt,
-                e.Skills.Select(s => new UserCharacterSkillDto(s.SkillId, s.Level, s.UpdatedAt)).ToList()
+                e.Skills.Select(s => new UserCharacterSkillDto(s.SkillId, s.Level, s.UpdatedAt)).ToList(), 
+                e.Equips.Select(s => new UserCharacterEquipDto(s.UserId, s.CharacterId, s.SlotId, s.ItemId)).ToList()
             );
     }
 
---
+++ WebServer/Domain/Entities/User/UserCharacterEquip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.User
+{
+    public sealed class UserCharacterEquip
+    {
+        public int UserId{ get; private set; }
+        public int CharacterId{ get; private set; }
+        public short SlotId { get; private set; }
+        public int? ItemId { get; private set; }
+        private UserCharacterEquip(){ }
+
+        public static UserCharacterEquip Create(int uesrId, int characterId, short slotId, int? itemId)
+            => new() { UserId = uesrId, CharacterId = characterId, SlotId = slotId, ItemId = itemId };
+
+
+        public void Equip(int itemId) => ItemId = itemId;
+        public void Unequip() => ItemId = null;
+    }
+}
